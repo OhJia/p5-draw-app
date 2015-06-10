94dd27413b2ca0480294c246266b82936092b793
OhJia <jiajia0919@gmail.com>
2015-06-10 18:10:21 -0400

    first commit

---
+++ platforms/ios/www/js/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+	var myCan = createCanvas(windowWidth, windowHeight);
+	myCan.parent('p5Container');
+}
+
+function draw() {
+	background(255, 200, 200);
+	color(255, 0, 0);
+	rect(100, 120, 50, 50);
+}
